560ca1af06261669f5c61237da2ac6bc2d5764d3
Adham Nour <adhamnourelwaffaa@gmaio.com>
2023-07-13 22:00:01 +0200

    Initial Commit

---
+++ AssignemntOne_ProblemOne/src/AssignemntOne_ProblemOne.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : AssignemntOne_ProblemOne.c
+ Author      : AdhamNour
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello Adham!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
